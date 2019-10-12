5c0f54c157a0355ee2930a1e598531f574e99425
lbw611 <547748891@qq.com>
2019-10-13 00:41:19 +0800

    lbw UI commit

---
+++ data/userInfo-data.js
@@ -0,0 +1,35 @@
+var local_database = [
+  { 
+    text: '我的活动',
+    url: 'myActivity/myActivity',
+    icon: '/image/userInfo/myActivity.png',
+    arrows: '/image/userInfo/arrow.png' 
+  },
+  { 
+    text: '活动记录', 
+    url: '#', 
+    icon: '/image/userInfo/activityRecord.png', 
+    arrows: '/image/userInfo/arrow.png' 
+  },
+  { 
+    text: '我的收藏', 
+    url: '#', 
+    icon: '/image/userInfo/shoucang.png', 
+    arrows: '/image/userInfo/arrow.png' 
+  },
+  {
+    text: '分享给朋友',
+    url: '#',
+    icon: '/image/userInfo/fenxiang.png',
+    arrows: '/image/userInfo/arrow.png'
+  },
+  {
+    text: '关于',
+    url: '#',
+    icon: '/image/userInfo/guanyu.png',
+    arrows: '/image/userInfo/arrow.png'
+  }
+]
+module.exports = {
+  postList: local_database
+}
